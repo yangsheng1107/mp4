ef80efc3313d9b89f0e6e5c06225e97b5d707e59
楊志昇 <yangsheng@ade-MacBook-Pro.local>
2016-03-29 23:10:46 +0800

    add sample code

--- main.c
+++ main.c
@@ -2,11 +2,34 @@
 
 int main(int argc, char *argv[])
 {
-    quicktime_t* qt;     
-    qt = quicktime_open("./lg.mp4", TRUE, FALSE, FALSE);
-    printf("quicktime_supported_video = %d \r\n",quicktime_supported_video(qt, 0));
-    printf("duration = %d \r\n",quicktime_sample_duration(qt->moov.trak[0]));
-    quicktime_close(qt);
+	int i, duration;
+    quicktime_t* qt;
+
+    qt = quicktime_open("test.mp4", TRUE, FALSE, FALSE);
+    printf("width = %d height = %d \r\n", qt->moov.trak[0]->mdia.minf.stbl.stsd.table->width, qt->moov.trak[0]->mdia.minf.stbl.stsd.table->height);
+
+	for(duration = 0, i = 0; i < qt->moov.trak[0]->mdia.minf.stbl.stts.total_entries; i++)
+	{
+		duration += qt->moov.trak[0]->mdia.minf.stbl.stts.table->sample_count * qt->moov.trak[0]->mdia.minf.stbl.stts.table->sample_duration;
+	}
+    printf("video duration = %ld \r\n",(duration/(qt->moov.trak[0]->mdia.mdhd.time_scale)));
+    printf("video duration = %ld \r\n",((qt->moov.trak[0]->mdia.mdhd.duration)/(qt->moov.trak[0]->mdia.mdhd.time_scale)));
+
+	for(duration = 0, i = 0; i < qt->moov.trak[1]->mdia.minf.stbl.stts.total_entries; i++)
+	{
+		duration += qt->moov.trak[1]->mdia.minf.stbl.stts.table->sample_count * qt->moov.trak[1]->mdia.minf.stbl.stts.table->sample_duration;
+	}
+    printf("audio duration = %ld \r\n",(duration/(qt->moov.trak[1]->mdia.mdhd.time_scale)));
+    printf("audio duration = %ld \r\n",((qt->moov.trak[1]->mdia.mdhd.duration)/(qt->moov.trak[1]->mdia.mdhd.time_scale)));
+
+    printf("sps/pps \r\n");
+	for(i = 0; i < qt->moov.trak[0]->mdia.minf.stbl.stsd.table->avcc.data_size; i++)
+	{
+		printf("0x%02x ", (unsigned char)qt->moov.trak[0]->mdia.minf.stbl.stsd.table->avcc.data[i]);
+	}
+    printf("\r\n");
+
+	quicktime_close(qt);
 
     return 0;
 }
